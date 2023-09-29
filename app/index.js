000ef84010aa67e188d8b0f5fab1500f6c0d313b
zayyid123 <mochamadzayyid@gmail.com>
2023-09-29 22:22:13 +0700

    add last read

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-import { Image, SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Page() {
   return (
@@ -7,14 +8,36 @@ export default function Page() {
       <View className='justify-center items-center'>
         <Image 
           source={require('../assets/image/quran.png')} 
-          className='w-[110px] h-[80px] '
+          className='w-[110px] h-[80px]'
         />
       </View>
 
       {/* last read */}
-      <View className='bg-gradient-to-r from-[#19b1ae] to-[#45fffc] p-2 mx-3 rounded-lg'>
-        <Text>Hello</Text>
-      </View>
+      <TouchableOpacity>
+        <LinearGradient
+          colors={['#19b1ae', '#45fffc']}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 1, y: 0.5 }}
+          className='p-3 mx-3 rounded-lg'
+        >
+          <View className='flex-row justify-between items-center'>
+            <View>
+              <Text className='text-white mb-3 font-light'>Last Read</Text>
+
+              {/* ayat */}
+              <Text className='text-white font-semibold text-base'>Al-Fatihah</Text>
+              <Text className='text-white font-light'>Ayat - No. 1</Text>
+              <Text className='text-white font-light'>Baca {'>'}</Text>
+            </View>
+
+            <Image 
+              source={require('../assets/image/last-read.png')}
+              className='w-[110px] h-[80px]'
+              resizeMode="contain"
+            />
+          </View>
+        </LinearGradient>
+      </TouchableOpacity>
     </View>
   );
 }
