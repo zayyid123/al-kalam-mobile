c10bde48e1e0227c35fbd4a6acd3d7a67b3a1cac
zayyid123 <mochamadzayyid@gmail.com>
2023-10-01 10:57:51 +0700

    implement darkmode to surah page

--- app/surah.js
+++ app/surah.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { GetAllDataSurah } from '../services/surahServices';
 import * as Font from 'expo-font';
+import { useColorScheme } from 'nativewind';
+
+// SVG
+import IconSearch from '../assets/icons/img_icon_search.svg'
 
 const SurahPage = () => {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   const [allDataSurah, setallDataSurah] = useState();
+  const [searchValue, setsearchValue] = useState('')
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   useEffect(() => {
@@ -27,7 +33,7 @@ const SurahPage = () => {
       <View
         className='flex-1 justify-center items-center'
       >
-        <Text className='font-bold text-lg'>Loading...</Text>
+        <Text className={`font-bold text-lg ${colorScheme === 'dark' && 'text-white'}`}>Loading...</Text>
       </View>
     )
   }
@@ -37,7 +43,7 @@ const SurahPage = () => {
       <View
         className='flex-1 justify-center items-center'
       >
-        <Text className='font-bold text-lg'>Loading...</Text>
+        <Text className={`font-bold text-lg ${colorScheme === 'dark' && 'text-white'}`}>Loading...</Text>
       </View>
     )
   }
@@ -46,9 +52,20 @@ const SurahPage = () => {
     <View className='mx-3'>
       {/* search */}
       <View
-        className='justify-center items-center'
+        className='mb-7 mt-1 items-center'
       >
-        <Text>Search</Text>
+        <View
+          className={`flex-row px-3 justify-between items-center ${colorScheme === 'dark' ? 'bg-[#32383d]' : 'bg-[#cbeef7]'} w-[70%] rounded-xl`}
+        >
+          <TextInput
+            className={`h-[50px] w-[70%] ${colorScheme === 'dark' ? 'text-white' : 'text-black'}`}
+            placeholder="Search"
+            placeholderTextColor={colorScheme === 'dark' ? '#fff' : 'black'}
+            onChangeText={newText => setsearchValue(newText)}
+            defaultValue={searchValue}
+          />
+          <IconSearch/>
+        </View>
       </View>
 
       {/* list surah */}
@@ -57,7 +74,9 @@ const SurahPage = () => {
       >
         {
           allDataSurah &&
-          allDataSurah.map((res, index) =>
+          allDataSurah
+          .filter((data) => data.nama_latin.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((res, index) =>
             <View 
               key={'item'+index}
               className='mb-6'
@@ -73,12 +92,20 @@ const SurahPage = () => {
                   <View
                     className='justify-center items-center'
                   >
-                    <Image
-                      source={require('../assets/image/penanda-hijau.png')}
-                      className='w-[34px] h-[34px]'
-                    />
+                    {
+                      colorScheme === 'dark' ?
+                      <Image
+                        source={require('../assets/image/penanda-putih.png')}
+                        className='w-[34px] h-[34px]'
+                      />
+                      :
+                      <Image
+                        source={require('../assets/image/penanda-hijau.png')}
+                        className='w-[34px] h-[34px]'
+                      />
+                    }
                     <Text
-                      className='absolute text-xs'
+                      className={`absolute text-xs ${colorScheme === 'dark' && 'text-white'}`}
                     >
                       {res.nomor}
                     </Text>
@@ -87,8 +114,8 @@ const SurahPage = () => {
                   <View
                     className='ml-3'
                   >
-                    <Text className='font-bold text-base'>{res.nama_latin}</Text>
-                    <Text className='font-thin text-sm'>{res.tempat_turun === 'mekah' ? 'Makiyah' : 'Madaniyah'} - {res.jumlah_ayat} Ayat</Text>
+                    <Text className={`font-bold text-base ${colorScheme === 'dark' && 'text-white'}`}>{res.nama_latin}</Text>
+                    <Text className={`font-thin text-sm ${colorScheme === 'dark' && 'text-[#32B0A8]'}`}>{res.tempat_turun === 'mekah' ? 'Makiyah' : 'Madaniyah'} - {res.jumlah_ayat} Ayat</Text>
                   </View>
                 </View>
 
@@ -96,7 +123,7 @@ const SurahPage = () => {
                 <View
                   className='justify-center'
                 >
-                  <Text style={{ fontFamily: 'LPMQIsepMisbah', fontSize: 20, color: '#166534' }}>{res.nama}</Text>
+                  <Text style={{ fontFamily: 'LPMQIsepMisbah', fontSize: 20, color: `${colorScheme === 'dark' ? '#fff' : '#166534'}` }}>{res.nama}</Text>
                 </View>
               </TouchableOpacity>
               <View style={{flex: 1, height: 0.5, backgroundColor: 'gray'}} />
